ae22fcaded8bfe85128e94a6eb5f8ef332dac49f
Michael John <mjbarton46@icloud.com>
2021-07-13 18:59:30 -0400

    Add basic UI for categories page

--- src/app/categories-list/categories-list.component.ts
+++ src/app/categories-list/categories-list.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { transactions } from '../transactions'
+
 @Component({
   selector: 'app-categories-list',
   templateUrl: './categories-list.component.html',
   styleUrls: ['./categories-list.component.css']
 })
 export class CategoriesListComponent implements OnInit {
+
+  transactions = transactions
 
   constructor() { }
 
