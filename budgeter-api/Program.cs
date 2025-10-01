8df3e96ba1e22e9f12ba5bc97842a0964883096d
michaeljohnbarton <mjbarton46@icloud.com>
2025-10-01 19:32:02 -0400

    Set up CORS policy for UI to call API

--- budgeter-api/Program.cs
+++ budgeter-api/Program.cs
@@ -1,6 +1,14 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "MyAllowSpecificOrigins",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod();
+        });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +23,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("MyAllowSpecificOrigins");
 
 app.UseHttpsRedirection();
 
