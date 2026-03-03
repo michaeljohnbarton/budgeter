9c5d777cfbf5a1fed3c0768e2679407bdc45e0ec
michaeljohnbarton <mjbarton46@icloud.com>
2026-03-03 18:58:45 -0500

    Remove unused tables

--- budgeter-database/sql_scripts/tables/MonthlyBalancePropogationType/seed.sql
+++
@@ -1,6 +0,0 @@
-USE budgeter;
-
-INSERT INTO dbo.MonthlyBalancePropogationType
-VALUES
-    (1, 'Bank Account'),
-    (2, 'Subcategory');
--- budgeter-database/sql_scripts/tables/MonthlyBudget/create.sql
+++
@@ -1,9 +0,0 @@
-USE budgeter;
-
-CREATE TABLE dbo.MonthlyBudget -- Will probably get rid of this in favor of doing "BudgetedAmount" in MonthlyBalance, wait to confirm
-(
-    ID INT PRIMARY KEY CLUSTERED,
-    MonthID INT NOT NULL FOREIGN KEY REFERENCES [Month](ID),
-    SubcategoryID INT NOT NULL FOREIGN KEY REFERENCES Subcategory(ID),
-    Amount decimal(9,2) NOT NULL
-);
